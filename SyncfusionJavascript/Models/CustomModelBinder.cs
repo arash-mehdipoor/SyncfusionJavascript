b6522cddcfb5cd492afa8fc874a46e3679b3c539
آرش مهدی پور <56753586+arash-mehdipoor@users.noreply.github.com>
2022-10-20 10:46:51 +0330

    Create CustomModelBinder.cs

---
+++ SyncfusionJavascript/Models/CustomModelBinder.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Request.Body.Peeker;
+using Syncfusion.EJ2.Base;
+
+namespace SyncfusionJavascript.Models;
+
+public class CustomModelBinder : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+
+        if (bindingContext == null)
+        {
+            throw new ArgumentNullException(nameof(bindingContext));
+        }
+
+        var modelType = bindingContext.ModelType;
+        var body = bindingContext.HttpContext.Request.PeekBody<DataManagerRequest>();
+
+        dynamic created = Activator.CreateInstance(modelType);
+        created.PageSize = body.Take;
+        created.PageNumber = 1;
+        created.SortAscending = true;
+        created.SortBy = body.Sorted?.FirstOrDefault().Name ?? "Id";
+
+        bindingContext.Result = ModelBindingResult.Success(created);
+        return Task.CompletedTask; 
+    }
+}
+
+public class CustomModelBinderProvider : IModelBinderProvider
+{
+    public IModelBinder GetBinder(ModelBinderProviderContext context)
+    {
+        return new CustomModelBinder();
+    }
+}
